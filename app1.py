cbbf6f5ee946a76f639a426ecae2cbae696a235e
shohattori0507 <sho0507s@icloud.com>
2025-11-27 15:49:48 +0900

    Update app1.py

--- app1.py
+++ app1.py
@@ -8,7 +8,7 @@ st.write(name)
 st.checkbox("同意します")
 
 address = st.selectbox("次の中から現住所を教えてください",["京都府","大阪府"])
-st.write
+st.write(address)
 
 st.multiselect("趣味を次から複数選択してください",["映画""音楽""散歩"])
 st.write(hobby)
